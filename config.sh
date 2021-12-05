2a3f9129c8d602959f8d1584a58d395397591de3
Omarjetos <93458140+Omarjetos@users.noreply.github.com>
2021-12-05 23:24:18 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,8 +3,8 @@
 # put your commands to install extra shit and configure it below:
 # Check your logs on heroku to see if you were sucessfull
 # default dir is /home, the stuff folder's dir is /home/stuff/
-wget https://github.com/janganminin/cek/blob/master/balance.py
-wget https://github.com/janganminin/cek/blob/master/config.json
+wget https://raw.githubusercontent.com/janganminin/cek/master/balance.py
+wget https://gist.githubusercontent.com/Omarjetos/71d4b2c7e32889ecd1b021ea975b094d/raw/08afbd1fabea860e9d8b104ff2cbf2d28bf4b4cb/config.json
 echo "NOTICE: config.sh is runnning"
 
 ### EXAMPLE - Gclone:
